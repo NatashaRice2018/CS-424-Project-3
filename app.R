6ddb5d8a45fd0cc9994447f0b9195dbbe3f77306
Natasha <natasharice2014@gmail.com>
2018-04-07 21:43:52 -0500

    C - table and chart showing the total numbers (and # and % in each magnitude) per month summed over all years

--- app.R
+++ app.R
@@ -56,8 +56,25 @@ ui <- dashboardPage(
                 box( title = "Tornados By Year", solidHeader = TRUE, status = "primary", width = 6,
                      plotOutput("stacked_bar_per_year")
                 )
+              ),
+              fluidRow(
+                box(title = "Tornadoes by Month", solidHeader = TRUE, status = "primary",width = 6,
+                    radioButtons("table_by_month_view", "Choose one:",  inline = TRUE,
+                                 choiceNames = list(
+                                   "Numaric Values",
+                                   "Percentages"
+                                 ),
+                                 choiceValues = list(
+                                   "numb", "perc"
+                                 )),
+                    dataTableOutput("table_per_month")),
+                
+                box( title = "Tornados By Month", solidHeader = TRUE, status = "primary", width = 6,
+                     plotOutput("stacked_bar_per_month")
+                )
               )
-      )
+              
+            )
       
     )
   )
@@ -118,6 +135,60 @@ server <- function(input, output) {
       
       
     }
+  )
+  
+  output$table_per_month<- DT::renderDataTable(
+    DT::datatable({
+      temp <- group_by(allData, month_abb, mag) %>% summarise(count = n()) %>% group_by(mag)
+      temp2 <- temp %>% complete(month_abb, mag) %>% group_by(month_abb) %>% fill(mag)
+      "fill 0's in to dataset"
+      temp2[is.na(temp2)] <- 0
+      "get data into correct form"
+      temp3 <- cast(temp2, month_abb ~ mag, mean, value = "count")
+      
+      "calculate totals"
+      temp3$month_total <-  rowSums(temp3[2:8])
+      
+      "calculate percent for each month"
+      
+      temp3$'-9 Percent' <- percent(temp3$`-9`/temp3$month_total)
+      temp3$'0 Percent' <- percent(temp3$`0`/temp3$month_total)
+      temp3$'1 Percent' <- percent(temp3$`1`/temp3$month_total)
+      temp3$'2 Percent' <- percent(temp3$`2`/temp3$month_total)
+      temp3$'3 Percent' <- percent(temp3$`3`/temp3$month_total)
+      temp3$'4 Percent' <- percent(temp3$`4`/temp3$month_total)
+      temp3$'5 Percent' <- percent(temp3$`5`/temp3$month_total)
+      
+      "move the total to last col"
+      temp3 <- temp3%>%select(-month_total,month_total)
+      
+      "Remove Col user does not want to see"
+      if(input$table_by_month_view == "numb")
+      {
+        finalChart <- temp3[-c(9:15)]
+      }
+      else
+      {
+        finalChart <- temp3[-c(2:8)]
+      }
+      
+      finalChart
+    },
+    options = list(pageLength = 12))
+  )
+  
+  output$stacked_bar_per_month<- renderPlot({
+    temp <- group_by(allData, month_abb, mag) %>% summarise(count = n()) %>% group_by(mag)
+    temp2 <- temp %>% complete(month_abb, mag) %>% group_by(month_abb) %>% fill(mag)
+    "fill 0's in to dataset"
+    temp2[is.na(temp2)] <- 0
+    
+    
+    ggplot(data=temp2, aes(x=month_abb, y=count, fill=mag)) +
+      geom_bar(stat="identity") + 
+      scale_fill_brewer(palette = "Set1")
+    
+  }
   )
   
 } 
